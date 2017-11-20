58d4b01c0b99ff8b7d1f67753174765a9aa492f8
Luke Campagnola <luke.campagnola@gmail.com>
2017-11-19 18:16:18 -0800

    Add initial jupyter widget code

---
+++ qpyter.js
@@ -0,0 +1,53 @@
+require.undef('qpyter');
+
+define('qpyter', ["jupyter-js-widgets"], function(widgets) {
+
+    var QpyterView = widgets.DOMWidgetView.extend({
+
+        initialize: function (parameters) {
+            QpyterView.__super__.initialize.apply(this, [parameters]);
+            this.model.on('msg:custom', this.on_msg, this);
+            this.model.on('change:image_data', this.image_changed, this);
+        },
+            
+        render: function() {
+            this.image = document.createElement('img');
+//             this.image.onload = this.resize_image.bind(this);
+            //this.image.src = "http://localhost:8888/static/base/images/logo.png";
+//             this.image.style = "border: 3px solid #F00; width: 200px; height: 200px;";
+            this.el.append(this.image);
+        },
+        
+        on_msg: function(msg) {
+            var b = new Blob([this.model.get('image_data').buffer]);
+            this.url = URL.createObjectURL(b);
+            this.image.src = this.url;
+        },
+
+        image_changed: function() {
+            console.log("image data changed");
+            console.log(self.model.get('image_data'));
+        },
+        
+//         resize_image: function() {
+//             console.log(this);
+//             console.log(this.image);
+//             
+//             this.image.width = this.image.naturalWidth;
+//             this.image.height = this.image.naturalHeight;
+//             console.log(this.image.height, this.image.naturalHeight);
+//         },
+//         
+        remove: function() {
+            // Inform Python that the widget has been removed.
+            this.send({
+                msg_type: 'status',
+                contents: 'removed'
+            });
+        },
+    });
+
+    return {
+        QpyterView: QpyterView
+    };
+});
---
+++ qpyter.py
@@ -0,0 +1,26 @@
+import os
+from IPython.display import display, Javascript
+import ipywidgets as widgets
+from traitlets import Bytes, Unicode
+
+
+# Send js widget definition to the browser
+jsfile = os.path.join(os.path.dirname(__file__), 'qpyter.js')
+js = open(jsfile, 'r').read()
+display(Javascript(js))
+
+
+class QpyterWidget(widgets.DOMWidget):
+    _view_name = Unicode('QpyterView').tag(sync=True)
+    _view_module = Unicode('qpyter').tag(sync=True)
+    image_data = Bytes('').tag(sync=True)
+    
+    def __init__(self, *args, **kwds):
+        widgets.DOMWidget.__init__(self, *args, **kwds)
+        self.on_msg(self.msg_received)
+
+    def msg_received(self, widget, content, buffers):
+        if content.get('msg_type') == 'status' and content.get('contents') == 'removed':
+            # widget was removed from notebook
+            print("Goodbye!")
+            
